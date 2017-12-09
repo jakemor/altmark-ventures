7e1af55285a00cd71a1f120519150c558923e69a
Jake Mor <jakemny@gmail.com>
2017-12-09 02:45:13 -0500

    done

---
+++ contact_us.php
@@ -0,0 +1,41 @@
+<div class="content" style="">
+
+	<h1 class="page-title">CONTACT US</h1>
+
+	<div class="our-focus">
+		<p>WE’RE ALWAYS HAPPY TO HEAR FROM YOU! GET IN TOUCH WITH US AND WE’LL BE SURE TO GET BACK TO YOU AS SOON AS WE CAN!</p>
+	</div>
+
+	<form class="form" action="http://formspree.io/jakemny@gmail.com" method="POST">
+		<div class="form-group row">
+			
+			<div class="col-md-4" class="form-col">
+				<label class="form-label" style="text-align: left;">First name</label>
+				<input required class="form-control" name="First" type="text" value="" placeholder="Jane">
+			</div>
+			
+			<div class="col-md-4" class="form-col">
+				<label class="form-label" style="text-align: left;">Last name</label>
+				<input required class="form-control" name="Last" type="text" value="" placeholder="Doe">
+			</div>
+			
+			<div class="col-md-4" class="form-col">
+				<label class="form-label" style="text-align: left;">email</label>
+				<input required class="form-control" name="Email" type="email" value="" placeholder="jane@example.com">
+			</div>
+
+			<div class="col-md-12" class="form-col">
+				<label class="form-label" style="text-align: left;">Talk to us!</label>
+				<textarea required name="Message" class="form-control" rows="5" placeholder="Let us know what's on your mind."></textarea>
+			</div>
+
+			<button type="submit" class="btn btn-default">Submit</button>
+
+		</div>
+	</form>
+
+	<div class="our-focus">
+		<p>WE LOOK FOR DRIVEN AND MOTIVATED ENTREPRENEURS THAT WANT TO BE IN THE FOREFRONT OF AN EMERGING INDUSTRY THAT ARE INTERESTED IN SOLVING COMPLEX BUT FUN PROBLEMS IN CREATIVE AND INTERESTING WAYS.</p>
+	</div>
+
+</div>
---
+++ home.php
@@ -0,0 +1,61 @@
+<style type="text/css">
+	
+#parent {
+    display: flex;
+    align-items: center;
+    height: 80vh;
+    padding: 2rem;
+}
+
+div.contact-blurb {
+	margin-top: 6rem;
+	text-align: left;
+	font-size: 0.75rem;
+	letter-spacing: 0.12rem;
+	line-height: 1.5rem;
+	position: fixed;
+	bottom: 30px;
+	left: 200px;
+	color: white;
+}
+
+@media only screen and (max-width: 1150px) {
+	div.contact-blurb {
+		text-align: center;
+		position: inherit;
+		margin-top: 5rem;
+		width: 100%;
+	}
+
+	#parent { 
+		height: 55vh;
+		margin-top: 6rem;
+	}
+}
+
+
+</style>
+
+
+<div id="parent">
+	<div id="child" class="center-vertical">
+		<img src="/static/img/logo.svg">
+		<br><br>
+		<div class="blurb">
+			<p><strong>ALTMARK VENTURES</strong>, <span style="opacity: 0.8;">founded in 2017, is a Private Investment fund focused exclusively on Blockchain tech, Algorithmic Trading, Human Trading, Mining, and various crypto assets.</span></p>
+		</div>
+	</div>
+
+
+</div>
+
+	<div class="contact-blurb">
+		<br>
+		<p>
+			<span style="opacity: 0.8;">NOW HIRING: <a class="contact-us" href='#'>CONTACT US</a></span> <br>
+			<span style="opacity: 0.8;">WANT TO TALK?: <a class="contact-us" href='#'>CONTACT US</a></span>
+		</p>
+
+	</div>
+
+
